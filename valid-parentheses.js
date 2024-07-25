2184a0ede71f38ed03721d08415487c92d9d5b81
Lyov-619 <lyovtigranyan@gmail.com>
2024-07-25 16:54:40 +0400

    This is my code but Tigran help me also

---
+++ valid-parentheses.js
@@ -0,0 +1,24 @@
+var isValid = function(s) {
+    if (s.length%2===1){
+        return false
+    }
+    let mstack = []
+    let mobj = {"{":"}","(":")","[":"]"}
+    for (let i=0; i<s.length; i++){
+        if (s[i] in mobj){
+            mstack.push(s[i])
+        }else{
+            if (mstack.length === 0){
+                return false
+            }
+            if (mobj[mstack[mstack.length-1]] !== s[i]){
+                return false
+            }else{
+                mstack.pop()
+            }
+        }
+    }
+    if (mstack.length > 0)
+        return false
+    return true
+};
